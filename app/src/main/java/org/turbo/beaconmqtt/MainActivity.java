48f056db9be8968453de62b0b74e75433069517d
Eve <eve.redero@gmail.com>
2023-02-08 11:41:25 +0100

    [BUILD] Build system autoupdate from Android Studio

--- app/src/main/java/org/turbo/beaconmqtt/MainActivity.java
+++ app/src/main/java/org/turbo/beaconmqtt/MainActivity.java
@@ -14,6 +14,7 @@ import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
